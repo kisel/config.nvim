94b2e9363adb29de960e366299cff3d75f29f9d5
Anton Kiselev <anton.kiselev@xored.com>
2024-10-17 16:55:56 +0700

    add :colo vscode

---
+++ lua/custom/plugins/extra-colorschemes.lua
@@ -0,0 +1,7 @@
+-- extra color schemes, not loaded by default
+return {
+    -- { "folke/tokyonight.nvim", priority = 1000 },
+    -- { "maxmx03/solarized.nvim", priority = 1000 },
+    { "Mofiqul/vscode.nvim", priority = 1000 },
+    -- { "ellisonleao/gruvbox.nvim", priority = 1000 },
+}
